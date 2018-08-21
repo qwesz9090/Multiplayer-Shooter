db5d7ec5c26863fd62863fc52928ec611ba1243b
Vilgot Jansson <viljan1ros@rosendal.local>
2018-08-21 11:03:31 +0200

    Makes communication possible.

--- client.py
+++ client.py
@@ -1,27 +1,25 @@
 import socket
 from _thread import *
 import sys
-ip = ""
+ip = "localhost"
 port = 5555
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-	s.bind((ip,port))
+	s.connect((ip,port))
 except socket.error as e:
 	print(str(e))
-s.listen(5)
-print ("Waiting for a connection with ip")
-def threaded_client(conn):
-	data = conn.recv(2048)
-	print (data.decode("utf-8"))
-	conn.send(str.encode("HEY: \n"))
-	data = conn.recv(2048)
-	print (data.decode("utf-8"))
-	
-	conn.close()
 
-while True:
-	conn,addr = s.accept()
-	print("Connected to: " +addr[0]+":"+str(addr[1]))
+print ("Waiting for a connection with server")
+data = s.recv(2048)
+print ("Connection Established")
+print ("Server: " + data.decode("utf-8") + "\n")
+try:
+	while True:
+		s.sendall(str.encode(input("Send: ")))
+		data = s.recv(2048)
+		print (data.decode("utf-8"))
+	s.close()
+except:
+	s.close()
 
-	start_new_thread(threaded_client,(conn,))
